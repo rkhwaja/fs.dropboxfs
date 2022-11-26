0c6d893d1568bf7acb11aa4ba177eaf9c4687457
Rehan Khwaja <rehan@khwaja.name>
2022-11-26 15:50:05 -0800

    Wrong package name

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-# Make the tests a module so that pytest will pick up the source code of the toodledo module for testing
+# Make the tests a module so that pytest will pick up the source code of the fs module for testing
 
 from os.path import join, realpath
 
